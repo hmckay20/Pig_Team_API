7aac9da0fd43bb7c17c87ed457e64e48c3568573
adahlman2 <adahlman2@huskers.unl.edu>
2024-02-20 16:29:11 -0600

    Added record counting for .sql file.

--- app/Http/Controllers/FeederIncrementalDataController.php
+++ app/Http/Controllers/FeederIncrementalDataController.php
@@ -19,7 +19,7 @@ class FileUploadController extends Controller
         ]);
         
         // Validate the uploaded file
-        $uploadedZipFile = $_FILES['zip_file']; // TODO: Change 'zip_file' to the real name of the file.
+        $uploadedZipFile = $_FILES['zip_file']; // TODO: Change 'zip_file' to the real name of the file?
         $zip = new ZipArchive();
 
         if ($zip->open($uploadedZipFile['tmp_name']) === TRUE) {
@@ -33,19 +33,32 @@ class FileUploadController extends Controller
             $message = "Failed to open the zip file.";
         }
 
-        // Is this today's data?
-        $currentDate = date("Y-m-d");
-        if (strstr($sqlFileName, $currentDate) !== false) {
-            // This is today's data
+        // Is this the table from yesterday?
+        $yesterdayDate = date("Y-m-d", strtotime("-1 day"));
+        if (strstr($sqlFileName, $yesterdayDate) !== false) {
+            // This is
+            // Check the number of records
+            $sqlDump = file_get_contents('sql_dump_file.sql'); // TODO: Change 'sql_dump_file.sql' to the path for the file
+
+            preg_match_all("/INSERT INTO `feeder_data_new_incremental` VALUES \((.*?)\);/", $sqlDump, $matches);
+            
+            $recordCount = 0;
+            foreach ($matches[1] as $insertValues) {
+                $values = explode('),(', $insertValues);
+                $recordCount += count($values);
+            }
+
+            // If there is no data, populate it first
+            // SEND AWAY
         } else {
-            // This is NOT today's data.
-        }
+            // This is NOT
+            // Send yesterday’s incremental table first. 
 
 
         // Add data to respective tables
         
 
         // Return a response based on validation and storing result
-        return response()->json(['message' => $message, 'path' => $zipPath]); //TODO: Add a $zipPath if necessary
+        return response()->json(['message' => $message, 'records' => $recordCount, 'path' => $zipPath]); //TODO: Add a $zipPath if necessary
     }
 }
