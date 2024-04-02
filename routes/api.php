0b9c23840a2db1e88e0597e9282b3317e672e4fa
Hunter <hmmmckay@gmail.com>
2024-04-02 14:17:43 -0500

    Deleted Dead Code

--- routes/api.php
+++ routes/api.php
@@ -5,7 +5,6 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HourlyStatusController;
 use App\Http\Controllers\ImageCaptureStatusController;
-use App\Http\Controllers\FileUploadController;
 use App\Http\Controllers\LogFilesController;
 
 /*
@@ -22,7 +21,6 @@ use App\Http\Controllers\LogFilesController;
 Route::post('/hourly-status', [HourlyStatusController::class, 'SendHourlyStatus']);
 Route::post('/image-capture-status', [ImageCaptureStatusController::class, 'CaptureStatus']);
 Route::post('/send-log-files', [LogFilesController::class, 'SendLogFiles']);
-Route::post('/upload-incremental', [FileUploadController::class, 'incrementalUpload']);
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
