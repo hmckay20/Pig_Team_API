333cf34b63843f5af74ef5f417ef2355abe6f9de
adahlman2 <adahlman2@huskers.unl.edu>
2024-04-23 15:26:15 -0500

    Prepping FileUploadController for working with .py script.

--- app/Http/Controllers/FileUploadController.php
+++ app/Http/Controllers/FileUploadController.php
@@ -8,23 +8,19 @@ use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Log;
 
-
-
 class FileUploadController extends Controller
 {
     public function incrementalUpload(Request $request)
     {
-        echo "Incremental table cleared. ";
+        clearIncremental(); // Ensure that the incremental table is cleared.
+
         Log::info('Start of incremental');
         $message = 'File uploaded successfully';
         $sqlFileName = NULL;
         $correctDate = FALSE;
         $correctCount = FALSE;
 
-       //$request->validate([ // Quick validate
-       //     'file' => 'required|mimes:zip',
-        //]);
-
+        // .zip file validation, opening, and retrieving data
         Log::info('I have validated the file ');
         if ($request->hasFile('zip_file')) {
             $uploadedZipFile = $request->file('zip_file');
@@ -32,8 +28,7 @@ class FileUploadController extends Controller
 
             if ($zip->open($uploadedZipFile->getRealPath()) === TRUE) {
                 if ($zip->numFiles == 1) {
-                    // Retrieve the name of the file inside the zip
-                    $sqlFileName = $zip->getNameIndex(0);
+                    $sqlFileName = $zip->getNameIndex(0); // Retrieve the name of the file inside the zip
                     Log::info("Zip file opened successfully. SQL file name: $sqlFileName");
 
                     // Read the content of the SQL file from the zip
@@ -56,18 +51,21 @@ class FileUploadController extends Controller
             return response()->json(['error' => 'No zip file uploaded or file does not meet the validation criteria.'], 400);
         }
         Log::info('fully validated the file ');
-        // Is this the table from yesterday?
+
+
+        // We only want to send the new data if yesterday's data has been sent.
         $yesterdayDate = date("Y-m-d", strtotime("-1 day"));
-        // This is NOT the table from yesterday.
-        if (strstr($sqlFileName, $yesterdayDate) == false) {
-            // DO NOT SEND DATA IF NOT FROM YESTERDAY
-            // Do action
-            $correctDate = TRUE;
+
+        if (checkPreviousData($yesterdayDate) == false) {
+            echo "This is not yesterday's table.";
+            die("This is not yesterday's table.");
         } else {
             $correctDate = TRUE;
         }
+
+        // The table is from yesterday.
         Log::info('I have validated that this file is from yesterday');
-        // This is the table from yesterday.
+
         // Check how many records were inserted in feeder_data_new yesterday
         $data_newServername = "127.0.0.1"; // feeder_data_new credentials
         $data_newUsername = "root";
@@ -86,13 +84,10 @@ class FileUploadController extends Controller
         if ($result) {
             $data_newCount = $result->num_rows;
         } else {
-
             die("Error executing query in feeder_data_new: " . $data_newConn->connect_error);
         }
 
         // Now check how many records feeder_data_new_incremental contains
-  // TODO: Change 'sql_dump_file.sql' to the path for the file?
-
         preg_match_all("/INSERT INTO `feeder_data_new_incremental` VALUES \((.*?)\);/", $sqlDump, $matches);
         $matchCount = count($matches[1]); // $matches[0] contains the full pattern matches
         Log::info("Number of matches found: {$matchCount}");
@@ -102,8 +97,7 @@ class FileUploadController extends Controller
             $recordCount += count($values);
         }
 
-        // If there is no data in incremental, try populating it first
-
+        // If there is no data in incremental, try populating it.
         if ($matchCount == 0 && $data_newCount != 0) {
             $data_newServername = "127.0.0.1"; // feeder_data_new credentials
             $data_newUsername = "root";
@@ -125,7 +119,7 @@ class FileUploadController extends Controller
                 die("Connection to feeder_data_new_incremental failed: " . $incrementalConn->connect_error);
             }
 
-            $sql = "INSERT INTO feeder_data_new_incremental SELECT * FROM feeder_data_new WHERE date LIKE '%$yesterdayDate%'";
+            $sql = "INSERT INTO feeder_data_new_incremental SELECT * FROM feeder_data_new WHERE id LIKE '%$yesterdayDate%'";
             $result = $incrementalConn->query($sql);
 
             $data_newConn->close();
@@ -138,7 +132,7 @@ class FileUploadController extends Controller
             }
         }
 
-        if ($matchCount == 3) { // Compare the counts
+        if ($matchCount == $data_newCount) { // Compare the counts
             $correctCount = TRUE;
         }
 
@@ -146,37 +140,40 @@ class FileUploadController extends Controller
           $this->sendIncrementalToMaster($sqlDump, $message);
           $this->populateFeederDataIds($message, $yesterdayDate, $matches, $recordCount, $sqlDump);
           $this->clearIncremental();
+        } else {
+            Log::info('There is no new data to send from day ' . $yesterdayDate);
+            $message .= "There is no new data to send from day " . $yesterdayDate . ". ";
         }
+
         Log::info('right before I send response');
-        // Return a response based on validation and storing result
-     //  return response()->json(['message' => $message, 'records' => $recordCount]); //TODO: Add a $zipPath if necessary
-        return response()->json(['I worked']);
+        return response()->json(['message' => $message, 'records' => $recordCount]);
     }
 
 
-    private function checkPreviousData($previousDate) // FIX
+    private function checkPreviousData($previousDate)
     {
-        // Check if the previous day's incremental table is there.
+        // Check if the previous day's incremental table has been updated. 
+        $truthServername = "127.0.0.1"; // data_sent_log credentials
+        $truthUsername = "root";
+        $truthPassword = "Untrusted";
+        $truthDatabase = "data_sent_log";
 
-        $masterServername = "127.0.0.1"; // master credentials
-        $masterUsername = "root";
-        $masterPassword = "Sun84Mus";
-        $masterDatabase = "feeder_data_new";
-
-        $masterConn = new \MySQLi($masterServername, $masterUsername, $masterPassword, $masterDatabase);
-        if ($masterConn->connect_error) {
-            die("Connection to master failed: " . $masterConn->connect_error);
+        $truthConn = new \MySQLi($truthServername, $truthUsername, $truthPassword, $truthDatabase);
+        if ($truthConn->connect_error) {
+            die("Connection to table data_sent_log failed: " . $truthConn->connect_error);
         }
 
-        $sql = "SELECT * FROM feeder_data_new WHERE date = '$previousDate'";
-        $result = $masterConn->query($sql);
+        $sql = "SELECT * FROM data_sent_log WHERE data_date = $date AND data_sent = 1";
+        $result = $truthConn->query($sql);
 
-        $masterConn->close();
+        $truthConn->close();
 
-        if ($result->num_rows == 0) { // Yesterday's data was not sent and received
-            // ACTION
+        if ($result->num_rows == 0) {
+            return FALSE;
+        } else {
+            $message .= "Data last updated on $date. ";
+            return TRUE;
         }
-        return response()->json(['Check previous data done']);
     }
 
 
@@ -269,8 +266,6 @@ class FileUploadController extends Controller
         }
 
         $idsConn->close();
-
-        return response()->json(['populated ids']);
     }
 
 
@@ -293,8 +288,6 @@ class FileUploadController extends Controller
         }
 
         $incrementalConn->close();
-
-        return response()->json(['cleared incremental']);
     }
 
 }
